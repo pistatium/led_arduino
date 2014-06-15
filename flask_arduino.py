86e42e878b0f44e8981ef265d9c1f885e7c688fe
kimihiro_n <kimihiro@Litvyak-4.local>
2014-06-15 18:30:50 +0900

    modify indent

--- flask_arduino.py
+++ flask_arduino.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return render_template('hello.html')
+    return render_template('hello.html')
 
 @app.route("/set_color",methods=['POST'])
 def set_color():
-	COM = "/dev/tty.usbmodem1421"
-	leds = LedArduino(COM);
-	c1 = request.values['color1']
-	c2 = request.values['color2']
-	leds.writeColorRGB(c1, 0)
-	leds.writeColorRGB(c2, 1)
-	return "success"
+    COM = "/dev/tty.usbmodem1421"
+    leds = LedArduino(COM);
+    c1 = request.values['color1']
+    c2 = request.values['color2']
+    leds.writeColorRGB(c1, 0)
+    leds.writeColorRGB(c2, 1)
+    return "success"
 
 
 if __name__ == "__main__":
